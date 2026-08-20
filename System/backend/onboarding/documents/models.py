14b6b1083dc50abe462c5f218490c34d3bcd3793
HwangInKyu <ic663688@gmail.com>
2026-08-20 16:36:38 +0900

    feat : documents/models & schedule/models 추가

--- System/backend/onboarding/documents/models.py
+++ System/backend/onboarding/documents/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounts.models import User
+from onboarding.models import OnboardingStep
 
-# Create your models here.
+class Documents(models.Model) :
+    """문서 - """
+
+    uploader =          models.ForeignKey(User, on_delete=models.PROTECT, related_name="documents_uploader", verbose_name="업로더 아이디")
+    onboarding_step =   models.ForeignKey(OnboardingStep, on_delete=models.SET_NULL, null=True, blank=True, related_name="documents_onboarding_step", verbose_name="온보딩 스텝 아이디")
+
+    document_title =    models.CharField(max_length=200, verbose_name="문서 제목")
+    file_path =         models.CharField(max_length=500, verbose_name="문서 경로")
+
+    class Meta :
+        db_table = "documents"
+
+    def __str__(self) :
+        return self.document_title
--- System/backend/onboarding/schedule/models.py
+++ System/backend/onboarding/schedule/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Schedule(models.Model) :
+    """스케줄"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="schedules", verbose_name="사용자 아이디")
+    schedule_title = models.CharField(max_length=100, verbose_name="스케줄 제목")
+    schedule_content = models.TextField(verbose_name="스케줄 내용")
+    schedule_date = models.DateField(verbose_name="스케줄 날짜")
+    is_shared = models.BooleanField(default=False, verbose_name="공유 여부")
+
+    class Meta :
+        db_table = "schedule"
+
+    def __str__(self) :
+        return f"{self.schedule_title} - {self.user.name} - {self.schedule_date}"
